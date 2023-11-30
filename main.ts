0a6e7036e42394806ff47b17ccdaaad83ce1ab90
Thisur <152498895+Thisur@users.noreply.github.com>
2023-11-30 21:18:52 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 140), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Heart)
+})
